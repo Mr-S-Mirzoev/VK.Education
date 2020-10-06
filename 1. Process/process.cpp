80c9cfbdd3e55b4406968dc7d1e9d086ca559722
Mr-S-Mirzoev <mrsergeymirzoev@gmail.com>
2020-10-06 23:14:08 +0100

    Added latest update: more code

--- 1. Process/process.cpp
+++ 1. Process/process.cpp
@@ -1,9 +1,69 @@
 #include "process.hpp"
 
 Process::Process(const std::string& path) {
-    pid = fork();
-    if (!pid) {
-        execl(path.c_str(), Process::exec_name(path).c_str(), nullptr);
+    int pipe1_fd[2];
+    int pipe2_fd[2];
+    int verif_fd[2];
+    pipe2(verif_fd, O_CLOEXEC);
+
+#ifdef DEBUG
+    std::cerr << path.c_str() << " " << Process::exec_name(path).c_str() << std::endl;
+#endif 
+    _pid = fork();
+    if (!_pid) {
+        signal(SIGINT, sig_handler);
+        execlp(path.c_str(), Process::exec_name(path).c_str(), nullptr);
+        return;
+    }
+    ::close(verif_fd[1]);
+
+    char tmp;
+    ssize_t size = ::read(verif_fd[0], &tmp, sizeof(tmp));
+
+    int status;
+#ifdef DEBUG
+    std::cerr << "PID: " << waitpid(_pid, &status, WNOHANG) << std::endl;
+    std::cerr << "Status: " << status << std::endl;
+#endif
+}
+
+Process::~Process() {
+    if (_pid) {
+        int status;
+        waitpid(_pid, &status, 0);
+#ifdef DEBUG
+        std::cerr << WEXITSTATUS(status) << std::endl;
+#endif
     }
 }
 
+void Process::close() {
+    if (_pid)
+        kill(_pid, SIGINT);
+    else {
+#ifdef DEBUG
+        std::cerr << "Trying to kill softly from child" << std::endl;
+#endif
+        exit(1);
+    }
+}
+
+void Process::closeStdin() {
+
+}
+
+size_t Process::read(void* data, size_t len) {
+    return 0;
+}
+
+void Process::readExact(void* data, size_t len) {
+
+}
+
+size_t Process::write(const void* data, size_t len) {
+    return 0;
+}
+
+void Process::writeExact(const void* data, size_t len) {
+
+}
--- 1. Process/process.hpp
+++ 1. Process/process.hpp
@@ -3,11 +3,28 @@
 
 #include <string>
 #include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
+#include <sys/wait.h>
+#include <signal.h>
+
+//#define DEBUG
+
+void sig_handler(int signal) {
+    exit(0);
+}
 
 class Process
 {
-    pid_t pid;
-    static std::string exec_name(const std::string& full_path);
+    pid_t _pid;
+    bool _noexec = false;
+
+    static std::string exec_name(const std::string& full_path) {
+        std::size_t pos = full_path.rfind('/');
+        if (pos == std::string::npos)
+            return full_path;
+        return full_path.substr(pos);
+    }
 public:
     explicit Process(const std::string& path);
     ~Process();
