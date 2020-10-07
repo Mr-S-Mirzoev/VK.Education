12e0bc6a3cbfd24099e7a709b292a88a93b8acaf
Sergey Mirzoev <s.mirzoev@avsw.ru>
2020-10-07 11:08:01 +0300

    TODO: make tests

--- 1. Process/main.cpp
+++ 1. Process/main.cpp
@@ -1,6 +1,23 @@
 #include "process.hpp"
 
-int main() {
-    Process ps("date");
+int main(int argc, char *argv[]) {
+    std::string path;
+    std::vector <std::string> args;
+    if (argc >= 2) {
+        path = argv[1];
+        for (int i = 2; i < argc; ++i)
+            args.push_back(argv[i]);
+    } else {
+        path = "ls";
+        args.push_back("-haltr");
+    }
+    Process ps(path, args);
+/*    std::string input;  // FOR CAT
+    std::cin >> input;
+    ps.write(input.data(), input.length());
+    std::string s(1000, '\0'); */
+    ps.read(s.data(), 1000);
+    std::cout << s << std::endl;
+    std::cout << ps.exec_failed() << std::endl;
     return 0;
 }
