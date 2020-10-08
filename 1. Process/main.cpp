165f75058074e7b4f705bfe53bb3ef89eef73b8d
Mr-S-Mirzoev <mrsergeymirzoev@gmail.com>
2020-10-08 18:06:59 +0100

    Fixed specified mistakes

--- 1. Process/main.cpp
+++ 1. Process/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "process.hpp"
 
 int main(int argc, char *argv[]) {
--- 1. Process/process.hpp
+++ 1. Process/process.hpp
@@ -2,27 +2,21 @@
 #define PROCESS_H
 
 #include <string>
-#include <vector>
-#include <iostream>
-
-#include <fcntl.h>
-#include <signal.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
+#include <vector>
 
 //#define DEBUG
+#define CHILD(pid) (pid == 0)
+#define POSIX_ERROR(ret_val) (ret_val < 0)
 
-void sig_handler(int signal);
+static std::vector<char*> arg_list(const std::string &name, const std::vector <std::string>& args);
+static std::string exec_name(const std::string& full_path);
 
 class Process
 {
     pid_t _pid;
-    bool _noexec = false;
-    int fd1[2];
-    int fd2[2];
-    
-    static std::vector<char*> arg_list(const std::string &name, const std::vector <std::string>& args);
-    static std::string exec_name(const std::string& full_path);
+    int fd_in, fd_out;
 public:
     explicit Process(const std::string& path, const std::vector <std::string>& args);
     ~Process();
@@ -36,7 +30,6 @@ public:
 
     void close();
 
-    bool exec_failed() const;
     int return_status() const;
 };
 
