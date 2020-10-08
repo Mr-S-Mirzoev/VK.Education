c48ea267631356e857cf19275dbb5f08dcc39838
Mr-S-Mirzoev <mrsergeymirzoev@gmail.com>
2020-10-08 18:06:59 +0100

    Fixed specified mistakes

--- 1. Process/test.h
+++ 1. Process/test.h
@@ -1,10 +1,16 @@
 #ifndef TEST_H
 #define TEST_H
 
+#include <iostream>
+
 #define TEST(name, count, code) \
     {\
         std::cout << "Test \"" << name << "\" #" << count << std::endl; \
-        code \
+        try { \
+            code \
+        } catch (std::runtime_error &re) { \
+            std::cout << "Exec failed: " << re.what() << std::endl; \
+        } \
         std::cout << std::endl; \
     }
 
